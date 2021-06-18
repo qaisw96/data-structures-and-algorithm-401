6ba53fd1ccabc366d9785e0dee5fbc6a319be212
Qais ata <73315519+qaisw96@users.noreply.github.com>
2021-06-18 17:18:04 +0300

    Update quick-sort.js

--- challenges/quickSort/quick-sort.js
+++ challenges/quickSort/quick-sort.js
@@ -1,24 +1,23 @@
 'use strict';
 
 function quickSort(arr) {
-    if(arr.length === 1) return arr
+    if(arr.length <= 1) return arr
+    // if(arr.length === 1 || arr.length ==0) return arr
 
     let pivot = arr[arr.length - 1]
     const leftArr = []
     const rightArr = []
 
-    for (let i = 0; i < arr.length - 1; i++) {
-       if(arr[i] < pivot) leftArr.push(arr[i])
-       else rightArr.push(arr[i])
+    for( const el of arr.slice(0, arr.length-1) ) {
+        el < pivot ? leftArr.push(el) : rightArr.push(el)
     }
 
-    if(leftArr.length > 0 && rightArr.length > 0) {
-        return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
-    } else if(leftArr.length > 0) {
-        return [...quickSort(leftArr), pivot]
-    }else {
-        return [pivot, ...quickSort(rightArr)]
-    }
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+    // if(leftArr.length > 0 && rightArr.length > 0) {
+    // } else if(leftArr.length > 0) {
+    //     return [...quickSort(leftArr), pivot]
+    // }else {
+    //     return [pivot, ...quickSort(rightArr)]
+    // }
 }
-
 module.exports = quickSort
